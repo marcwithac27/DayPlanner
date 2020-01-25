8c6e91a370af20e97e58bd51d95d5264df02e526
Marc Maldonado <marc@telecomelite.com>
2020-01-25 07:30:29 -0500

    got moment function working kinda

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var taskholder = document.getElementById('nine')
 taskholder.textContent = localStorage.getItem('storedTask')
+var time = moment().format('LLLL')
 
+timeEl = document.getElementById('currentDay')
+timeEl.append(time)
+console.log(time)
 $('button').on('click', function(){
 var storedTask = $('input').val ();
 localStorage.setItem('storedTask', storedTask)
