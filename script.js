ef3a621a2888cd0ee3f7375b71cd35b04bee85b0
Marc Maldonado <marc@telecomelite.com>
2020-01-25 07:47:17 -0500

    buttons kinda work lol

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var taskholder = document.getElementById('nine')
-taskholder.textContent = localStorage.getItem('storedTask')
-var time = moment().format('LLLL')
+var taskholder = document.getElementById('nine');
+taskholder.textContent = localStorage.getItem('storedTask');
+var time = moment().format('LLLL');
 
-timeEl = document.getElementById('currentDay')
-timeEl.append(time)
-console.log(time)
+timeEl = document.getElementById('currentDay');
+$('#currentDay').append(time);
+console.log(time);
 $('button').on('click', function(){
 var storedTask = $('input').val ();
-localStorage.setItem('storedTask', storedTask)
-taskholder.textContent = localStorage.getItem('storedTask')
-console.log(taskholder.text)
+localStorage.setItem('storedTask', storedTask);
+taskholder.textContent = localStorage.getItem('storedTask');
+
 })
